4e2f8fc4008a36e96ff235dbac72463253d09d2a
Jordan <mcgalliard.jordan@gmail.com>
2022-08-20 17:29:56 -0600

    Setup project. Home page, router, file structure.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>Animations, transitions, transforms, keyframes, etc. Demo</h1>
+      
+      <p>This project includes some different things I've made to practice
+        CSS properties that have to do with animations. I also practiced React Router
+        when making this project.
+      </p>
+
+      <ul>
+        <li>Top-menu toggle</li>
+        <li>Side-menu toggle</li>
+        <li>Image revolver</li>
+        <li>Side-swipe modal</li>
+      </ul>
+   
     </div>
   );
 }
